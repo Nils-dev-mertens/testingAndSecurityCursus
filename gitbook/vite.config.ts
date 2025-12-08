3b5a94145d7f0c9b42eda7842f5712f41b3aff1a
Nils-dev-mertens <nilsm2950@gmail.com>
2025-12-08 10:11:27 +0000

    updated the allowedhost

--- gitbook/vite.config.ts
+++ gitbook/vite.config.ts
@@ -7,9 +7,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-    resolve: {
+  resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     }
+  },
+  server: {
+    allowedHosts: true,
   }
 })
